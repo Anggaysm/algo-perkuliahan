c896fdbc88520060d3bcc9e61e7a3ba77e4174ac
Anggaysm <prasetiaangga28@gmail.com>
2022-12-27 13:49:02 +0700

    tambah satu

--- project-akhir-taun/parkir.cpp
+++ project-akhir-taun/parkir.cpp
@@ -2,41 +2,74 @@
 using namespace std;
 
 int biaya(){
-    int jam, sejam;
+    int jam, sejam, total;
+    float potongan;
     cout << "Lama Parkir /jam : ";
     cin >> jam;
+
     
 
     for(int i = 1; i < jam; i++){
         sejam += 2000;
     }
-    return sejam;
-}
-
-void motor(){
-    int pertama = 2000 + biaya();
-    cout << "Total biaya parkir : Rp" << pertama;
-}
-
-void mobil(){
-    int pertama = 5000 + biaya();
-    cout << "Total biaya parkir : Rp" << pertama;
-}
-int main(){
-    int jenis;
-    cout << "1. Motor"<<endl<< "2. Mobil"<< endl << "Pilih kendaraan : ";
-    cin >> jenis;
-    switch (jenis)
+    char member;
+    cout << "Apakah punya kartu parkir member?(y/n)";
+    cin >> member;
+    if (member == 'y')
     {
-    case 1:
-        motor();
-        break;
-    
-    case 2:
-        mobil();
-        break;
-    
-    default:
-        cout << "Kendaraan tidak cocok, Parkir geratis";
+        potongan = (sejam+2000) * 0.2;
+        total = sejam - potongan;
+    }else if (member == 'n')
+    {
+        total = sejam;
     }
+    
+    return total;
+}
+
+int motor(){
+    int pertama = 2000 + biaya();
+    cout << "biaya motor "<< " :Rp " <<pertama<<endl;
+    return pertama;
+}
+
+int mobil(){
+    int pertama = 5000 + biaya();
+    cout << "biaya mobil "<< " :Rp " <<pertama<<endl;
+    return pertama;
+}
+
+
+
+int totalKendaraan(){
+    int totalKendaraan;
+    cin >> totalKendaraan;
+    int i, hargaunit, totalHarga = 0;
+    int tipe [totalKendaraan];
+    for ( i=0; i<totalKendaraan; i++){
+        cout << "1. Motor"<<endl<< "2. Mobil"<< endl << "Tipe Kendaraan : ";
+        cin >> tipe[i];
+        switch (tipe[i])
+        { 
+        case 1:
+            hargaunit = motor();
+            break;
+        case 2:
+            hargaunit = mobil();
+            break;
+        
+        default:
+            cout << "Kendaraan tidak cocok, Parkir gratis" << endl;
+        }
+        totalHarga += hargaunit;
+    }
+    cout << "Total penghasilan hari ini: Rp";
+    return totalHarga;
+
+}
+
+
+int main(){
+    cout << "Total Kendaran Hari ini : ";
+    cout << totalKendaraan();
 }
---
+++ project-akhir-taun/supermarket.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+int harga(){
+    int hargabarang[5] = {10000,7000,20000,3000,15000};
+    int kode;
+    cin >> kode;
+    return hargabarang[kode-1];
+}
+
+int perhitungan(int jumlahbrg, int hargabrg){
+    return (jumlahbrg*hargabrg);
+}
+
+
+int kembalian(int uangPembeli, int hargaBarang){
+    if (uangPembeli < hargaBarang)
+    {
+        cout << "Uang anda Kurang";
+    }else if(uangPembeli == hargaBarang){
+        cout << "Uang Pas tidak ada kembalian";
+    }else{
+        return uangPembeli - hargaBarang;
+    }
+    return 0;
+}
+
+
+
+int main(){
+    int total = 0;
+    jump:
+    string barang [5] = {"Oreo", "es krim", "baigon", "mineral","keripik"};
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << i+1 << ". "<< barang[i]<<endl;
+    }
+    cout << "Pilih barang yang ingin anda beli (1-5) :";
+    int hargaBrg = harga();
+
+
+    int jumlah;
+    cout << "Jumlah barang: ";
+    cin >> jumlah;
+    int hitung;
+    hitung = perhitungan(jumlah, hargaBrg);
+    cout << hitung<< endl;
+    
+    total += hitung;
+    char lagi;
+    cout << "Apakah ada yang ingin dibeli lagi ? (y/n)";
+    cin >> lagi;
+    if (lagi == 'n'){
+        cout << "Total belanjaan Anda : Rp" << total<< endl;
+        int uang;
+        cout << "Uang Pembeli : Rp";
+        cin >> uang;
+        int kembali = kembalian(uang, total);
+        cout << "Kembalian anda adalah : Rp"<< kembali<< endl;
+        cout << "Terimakasi Telah Belanja di Sini :)";
+    }else{
+        goto jump;
+    }
+    
+}   
